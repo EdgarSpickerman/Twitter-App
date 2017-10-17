7c1fd1bdd76992d9b1eea6b69dfe92e0b0e4c377
edgar <spickerman.edgar@gmail.com>
2017-10-16 21:54:46 -0700

    authenticated user and retrieved data general user data from twitter's restful api

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config.js');
+const Twit = require('twit');
+let error = '';
+
+//Authenticates the newly created twit
+//resolves with the authUser's name, screen_name, friends count, profile image and banner;
+//rejects the user if any key is invalid.
+const isAuthenticated = keys => {
+    return new Promise((resolve,reject) => {
+        let T = new Twit(keys)
+        T.get('account/verify_credentials', (err, data) => {
+            if (err) return reject(err)
+            let authUser = {};
+            authUser.id = data.id;
+            authUser.name = data.name;
+            authUser.screenName = data.screen_name;
+            authUser.picture = data.profile_image_url;
+            authUser.banner = data.profile_banner_url;
+            authUser.friendsNum = data.friends_count;
+            resolve(authUser);
+        });
+    });
+}
 
 
+//Describes what happens when the server recieves a get request to the / route.
+//authenticates the user retrieving the last 5 tweets,DM's, and friends rendering that info to the screen.
+//reject's the user's request if any error's are present with a friendly message
 router.get('/', (req, res) => {
-    res.render('index', {})
+    isAuthenticated(config)
+        .then(user => {
+            console.log(user);
+            res.render('index')
+        }).catch(err => {
+            console.log(err.message);
+            //error = err.message might have to dynamically write the error to /error
+            res.redirect('/error');
+        });
 });
 
 
